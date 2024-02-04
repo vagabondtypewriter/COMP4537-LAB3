07d11aa4a6228bf8d4bc8e80caff6707203d067e
Kiefer Thom <97416146+vagabondtypewriter@users.noreply.github.com>
2024-02-04 12:43:59 -0800

    Add files via upload

---
+++ js/server.js
@@ -0,0 +1,22 @@
+const utils = require('../modules/utils.js');
+const http = require('http');
+const url = require('url');
+const STRINGSTORE = require('../lang/messages/en/en.js')
+
+http.createServer(function (req, res) {
+  let q = url.parse(req.url, true);
+  res.writeHead(200, {'Content-type' : 'text/html', 'Access-Control-Allow-Origin' : '*'});
+  // create html response with blue-colored text
+  let htmlResponse;
+  if (q.query && q.query.name) {
+    htmlResponse = `<div style="color: blue;">${STRINGSTORE.GREETING_MSG.replace('%s', q.query.name)}${utils.getDate()}</div>`;
+  }
+  else {
+    htmlResponse = `<div style="color: blue;">It is now: ${utils.getDate()}</div>`;
+    
+  }
+  res.write(htmlResponse);
+  res.end();
+}).listen(8888);
+
+
---
+++ lang/messages/en/en.js
@@ -0,0 +1,5 @@
+const STRINGSTORE = {
+  GREETING_MSG : "Hello %s, wonderful weather! Server time and date is: "
+}
+
+module.exports = STRINGSTORE;
